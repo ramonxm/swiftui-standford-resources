d42bfb186d482510e5d8a62bfdcf368b05591d1a
Ramon Xavier <ramon1234rxm@gmail.com>
2024-03-27 22:04:05 -0300

    feat: initial project (memorize) with swiftui

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ramon Xavier on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
