22b62554ef10517d1d329d409cbbae96c7969b5f
Ramon Xavier <ramon1234rxm@gmail.com>
2024-06-21 20:19:19 +0200

    feat: chosen index faceup card

--- Memorize/Memorize/MemorizeGame.swift
+++ Memorize/Memorize/MemorizeGame.swift
@@ -24,19 +24,23 @@ struct MemorizeGame<CardContent> where CardContent: Equatable {
     
     mutating func choose(_ card: Card) {
         if let chosenIndex = cards.firstIndex(where: { $0.id == card.id}) {
-            
             if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched {
-                
                 if let potentialMatchIndex = indexOfTheOneAndOnlyFaceUpCard {
                     if cards[chosenIndex].content == cards[potentialMatchIndex].content {
                         cards[chosenIndex].isMatched = true
                         cards[potentialMatchIndex].isMatched = true
                     }
+                    indexOfTheOneAndOnlyFaceUpCard = nil
+                } else {
+                    for index in cards.indices {
+                        cards[index].isFaceUp = false
+                    }
+                    
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
                 }
-                
                 cards[chosenIndex].isFaceUp = true
             }
-      }
+        }
     }
     
     mutating func shuffle() {
