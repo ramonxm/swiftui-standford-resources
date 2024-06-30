394e908717e25807d27fbc94bcbaa08ebe14ca75
Ramon Xavier <ramon1234rxm@gmail.com>
2024-06-30 14:43:47 -0300

    feat: create aspect v grid view

--- Memorize/Memorize/EmojiMemorizeGameView.swift
+++ Memorize/Memorize/EmojiMemorizeGameView.swift
@@ -10,32 +10,12 @@ import SwiftUI
 struct EmojiMemorizeGameView: View {
     @ObservedObject var viewModel: EmojiMemorizeGame
     private let aspectRatio: CGFloat = 2/3
-
-    func gridItemWidthThatFits(count: Int, size: CGSize, atAspectRatio aspectRatio: CGFloat) -> CGFloat {
-        let count = CGFloat(count)
-        var columnCount = 1.0
-        
-        repeat {
-            let width = size.width / columnCount
-            let height = width / aspectRatio
-            
-            let rowCount = (count / columnCount).rounded(.up)
-            
-            if rowCount * height < size.height {
-                return (size.width / columnCount).rounded(.down)
-            }
-            
-            columnCount += 1
-        } while columnCount < count
-        return min(size.width / count, size.height * aspectRatio).rounded(.down)
-    }
-
     
     var body: some View {
         VStack {
             cards
                 .animation(.default, value: viewModel.cards)
-        
+            
             Button("Shuffle") {
                 viewModel.shuffle()
             }
@@ -43,27 +23,18 @@ struct EmojiMemorizeGameView: View {
         .padding()
     }
     
-    var cards: some View {
-        GeometryReader { geometry in
-            let gridItemSize = gridItemWidthThatFits(
-                count: viewModel.cards.count,
-                size: geometry.size,
-                atAspectRatio: aspectRatio)
-            
-            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
-                ForEach(viewModel.cards) { card in
-                    CardView(card)
-                        .aspectRatio(aspectRatio, contentMode: .fit)
-                        .padding(4)
-                        .onTapGesture {
-                            viewModel.choose(card)
-                        }
-                    }
+    private var cards: some View {
+        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
+            CardView(card)
+                .aspectRatio(aspectRatio, contentMode: .fit)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
                 }
-            }
-            .foregroundColor(Color.orange)
         }
+        .foregroundColor(Color.orange)
     }
+}
 
 struct CardView: View {
     let card: MemorizeGame<String>.Card
